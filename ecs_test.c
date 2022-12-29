ac9f51c8d526bfa8db548fd91faa0f066c71d812
RestoneAGX <redstonegameraa@gmail.com>
2022-12-29 07:17:40 -0500

    All ECS types have Add and Remove funcs | Also, created a nice macro to easily duplicate the functions

---
+++ ecs_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "tools/v2/ECS2.h"
+
+int main(){
+
+    return 0;
+}
--- tools/v2/ECS2.h
+++ tools/v2/ECS2.h
@@ -1,4 +1,5 @@
 #pragma once
+#include<SDL2/SDL.h>
 
 #define BitCheck(v,n) (v & (1 << n))
 #define BitToggle(v,n) (v ^ (1 << n))
@@ -10,61 +11,75 @@
 #define ENTITY_BUFF_LEN 60
 #define PROJECTILE_BUFF_LEN 255
 
-typedef struct ItemBuffer {
+typedef struct itemBuffer {
     unsigned char current_idx;
     unsigned char id[ITEM_BUFF_LEN];
     unsigned char amount[ITEM_BUFF_LEN];
     SDL_Rect *src[ITEM_BUFF_LEN];
     SDL_FRect sprite[ITEM_BUFF_LEN];
-}
+}itemBuffer;
 
-typedef struct Components { unsigned char Hp, Atk, Def, x, y } components;
+typedef struct Components { unsigned char Hp, Atk, Def, x, y; } Components;
 
-typedef struct EntityBuffer {
+typedef struct entityBuffer {
     unsigned char current_idx;
     unsigned char id[ENTITY_BUFF_LEN];
     SDL_Rect *src[ENTITY_BUFF_LEN];
     Components components[ENTITY_BUFF_LEN]; 
     SDL_FRect sprite[ENTITY_BUFF_LEN];
-}
+}entityBuffer;
 
-typedef struct ProjectileBuffer {
+typedef struct projectileBuffer {
     unsigned char current_idx;
+    unsigned char id[PROJECTILE_BUFF_LEN];
     unsigned char effects[PROJECTILE_BUFF_LEN];
     SDL_Rect *src[PROJECTILE_BUFF_LEN];
     SDL_FRect sprite[PROJECTILE_BUFF_LEN];
-}
+}projectileBuffer;
 
 typedef struct item{
    unsigned char id;
    unsigned char amount;
    SDL_Rect *src;
    SDL_FRect sprite; 
-}
+}item;
+
 typedef struct entity{
     unsigned char id;
     SDL_Rect *src;
     Components components;
     SDL_FRect sprite;
-}
+}entity;
 
 typedef struct projectile {
+    unsigned char id;
     unsigned char effects;
     SDL_Rect *src;
     SDL_FRect sprite;
-}
+}projectile;
 
 #define cIdx buff->current_idx
-void add_entity(EntityBuffer* buff, entity* entity_presets, unsigned char id){
-    buff->id[cIdx] = id;
-    buff->src[cIdx] = entity_presets[id].src;
-    buff->components[cIdx] = entity_presets[id].components;
-    buff->sprite[cIdx++] = entity_presets[id].sprite;
+#define BUFFER_FUNCS(val, sp_case, reverse_sp_case) \
+void add_##val (val##Buffer* buff, val * val##_presets, unsigned char id){ \
+    buff->id[cIdx] = id; \
+    buff->src[cIdx] = val##_presets[id].src; \
+    buff->sprite[cIdx++] = val##_presets[id].sprite; \
+    sp_case; \
+} \
+void remove_##val (val##Buffer* buff, unsigned char idx){ \
+    buff->id[idx] = buff->id[--cIdx]; \
+    buff->src[idx] = buff->src[cIdx]; \
+    buff->sprite[idx] = buff->sprite[cIdx]; \
+    reverse_sp_case; \
 }
 
-void remove_entity(EntityBuffer* buff, unsigned char idx){
-    buff->id[idx] = buff->id[--cIdx];
-    buff->src[idx] = buff->src[cIdx];
-    buff->components[idx] = buff->components[cIdx];
-    buff->sprite[idx] = buff->sprite[cIdx];
-}
+
+BUFFER_FUNCS(item,
+             buff->amount[cIdx] = item_presets[id].amount,
+             buff->amount[idx] = buff->amount[cIdx])
+
+BUFFER_FUNCS(entity, 
+             buff->components[cIdx] = entity_presets[id].components,
+             buff->components[idx] = buff->components[cIdx])
+
+BUFFER_FUNCS(projectile, , )
